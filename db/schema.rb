dc8c1af43f860976457c334eab03c87d7b1bf65d
Safal Acharya <acharyasafal51@gmail.com>
2026-01-15 14:05:29 +0545

    Added new column status

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_14_142146) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_15_081738) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
+    t.integer "status", default: 0, null: false
     t.string "title"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
